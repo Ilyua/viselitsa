fdedb6ffd7c2a80c663f45fa8671ad10f2e084af
Ilyua <old.flanigan@gmail.com>
2017-03-12 14:09:02 +0300

    Initial version

---
+++ README.md
@@ -0,0 +1,10 @@
+The North Centre Positronics (с)
+
+###Program "Magic ball"(8 ball)
+---------------------------------------------------
+
+***How to use?***
+
+
+Run 8ball.rb in console
+Ask something and program will help you to make a solution
---
+++ word_reader.rb
@@ -0,0 +1,20 @@
+require "unicode_utils/downcase"
+
+class WordReader
+  def read_from_args
+    return ARGV[0]
+  end
+
+  def read_from_file(file_name)
+
+    if !File.exist?(file_name)
+      return nil
+    end
+
+    file = File.new(file_name, "r:UTF-8")
+    lines = file.readlines
+    file.close
+
+    return UnicodeUtils.downcase(lines.sample.chomp)
+  end
+end
