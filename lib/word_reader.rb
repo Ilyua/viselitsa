380036bc03eade4b1a0703c5d602113fdc5d8b3e
Ilyua <old.flanigan@gmail.com>
2017-03-20 14:41:01 +0300

    Some refactor

--- lib/word_reader.rb
+++ lib/word_reader.rb
@@ -4,11 +4,8 @@ class WordReader
   end
 
   def read_from_file(file_name)
-    unless File.exist?(file_name)
-      return nil
-    end
+    return nil unless File.exist?(file_name)
     lines = File.readlines(file_name)
-    p lines
     return lines.sample.chomp
   end
 end
